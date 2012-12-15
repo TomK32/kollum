077a1d3a8077e16693830ed8e476591139e58714
Thomas R. Koll <tomk32@tomk32.de>
2012-12-15 22:48:15 +0100

    Swap images

--- animations.lua
+++ animations.lua
@@ -4,9 +4,9 @@ animations.valve = newAnimation(love.graphics.newImage("images/valve.png"), 32, 
 animations.valve:setMode("loop")
 
 animations.villain = {}
-animations.villain.standing = newAnimation(love.graphics.newImage("images/tombo_standing.png"), 32, 32, 0.3, 0)
+animations.villain.standing = newAnimation(love.graphics.newImage("images/kollum_standing.png"), 32, 32, 0.3, 0)
 
 animations.hero = {}
-animations.hero.standing = newAnimation(love.graphics.newImage("images/kollum_standing.png"), 32, 32, 0.3, 0)
+animations.hero.standing = newAnimation(love.graphics.newImage("images/tombo_standing.png"), 32, 32, 0.3, 0)
 return animations
 
